91c8ef1cdd2d0e8d67b2e41a7ed732ef39618a3d
Merissa Bridgeman <merissabridgeman@Merissas-MacBook-Pro-2.local>
2022-02-11 22:45:49 -0800

    completed check cholesterol level

--- exercises/simplify-conditionals/check_cholesterol_levels.py
+++ exercises/simplify-conditionals/check_cholesterol_levels.py
@@ -3,25 +3,30 @@
 # Reference: https://www.healthline.com/health/high-cholesterol/levels-by-age
 
 # Blood test analysis program
-total_cholostrol = 70
-ldl = 30
-triglyceride = 120
+total_cholostrol = 230
+ldl = 150
+triglyceride = 170
 
-if total_cholostrol < 200 and ldl < 100 and triglyceride < 150:
-    # good level
-    print('*** Good level of cholestrol ***')
-elif 200 < total_cholostrol > 240 or ldl > 160 or triglyceride >= 200:
-    # High cholestrol level
+def high_cholesterol_alert():
     print('*** High cholestrol level ***')
     print('start taking pills such as statins')
     print('start TLC diet')
-elif 200 <total_cholostrol < 240 or 130 < ldl < 160 or 150 <= triglyceride < 200:
-    #TLC_diet
+def tlc_diet_alert():
     print('*** Borderline to moderately elevated ***')
     print("Start TLC diet")
     print("Under this meal plan, only 7 percent of your daily calories \nshould come from saturated fat.")
     print('Some foods help your digestive tract absorb less cholesterol. For example,\nyour doctor may encourage you to eat more:')
     print('oats, barley, and other whole grains.')
     print('fruits such as apples, pears, bananas, and oranges.')
+
+if total_cholostrol < 200 and ldl < 100 and triglyceride < 150:
+    # good level
+    print('*** Good level of cholestrol ***')
+elif 200 < total_cholostrol > 240 or ldl > 160 or triglyceride >= 200:
+    # High cholestrol level
+    high_cholesterol_alert()
+elif 200 <total_cholostrol < 240 or 130 < ldl < 160 or 150 <= triglyceride < 200:
+    #TLC_diet
+    tlc_diet_alert()
 else:
     print('Error: unhandled case.')
--- exercises/simplify-conditionals/extract_position.py
+++ exercises/simplify-conditionals/extract_position.py
@@ -3,16 +3,16 @@
 
 def extract_position(line):
     if not line:
-        pos = None
+        return None
     else:
         if 'debug' in line or 'error' in line:
-            pos = None
+            return None
         else:
             if 'x:' in line:
                 start_index = line.find('x:') + 2
                 pos = line[start_index:] # from start_index to the end.
             else: 
-                pos = None
+                return None
     return pos
 
 if __name__ == "__main__":
